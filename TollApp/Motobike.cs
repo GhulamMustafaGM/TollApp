55dcaca43d72b2c648a1d1775bf5dbaf3a2d378b
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-17 21:00:01 +0100

    Toll Free Calculator App

---
+++ TollApp/Motobike.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollApp
+{
+    public class Motorbike : Vehicle
+    {
+        // public string GetVehicleType()
+        // {
+        //     return "Motorbike";
+        // }
+
+        public bool IsTollFreeVehicle()
+        {
+            return true;
+        }
+    }
+}
---
+++ TollApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I have tested and checked the results in the following 3 different dates. Results are correct. 
+            DateTime[] date = { new DateTime(2021, 08, 17, 6, 25, 0), new DateTime(2021, 08, 17, 7, 40, 0), new DateTime(2021, 08, 17, 15, 40, 0), new DateTime(2021, 08, 17, 17, 5, 0) };
+            // DateTime[] date = { new DateTime(2021, 07, 17, 6, 25, 0), new DateTime(2021, 07, 17, 7, 40, 0), new DateTime(2021, 07, 17, 15, 40, 0), new DateTime(2021, 07, 17, 17, 5, 0) };
+            // DateTime[] date = { new DateTime(2021, 04, 30, 6, 25, 0), new DateTime(2021, 04, 30, 7, 40, 0), new DateTime(2021, 04, 30, 15, 40, 0), new DateTime(2021, 04, 30, 17, 5, 0) };
+            TollCalculator toll = new TollCalculator();
+            int tollFee = toll.GetTollFee(new Car(), date);
+            // int tollFee = toll.GetTollFee(new Motorbike(), date);
+            Console.WriteLine("Today, total toll fee collected is :  " + tollFee);
+            Console.ReadLine();
+        }
+    }
+}
